58c33617ccb893bfe56301d0e0b77686c2ebd3c2
JelenaKlenkar <jelena.klenkar@gmail.com>
2019-07-12 22:48:24 +0200

    added SetTreatment Class to doctor package

---
+++ src/doctor/SetTreatment.java
@@ -0,0 +1,14 @@
+package doctor;
+
+public class SetTreatment {
+
+	public static void main(String[] args) {
+		// create new object
+
+		Treatment treatment = new Treatment();
+
+		treatment.setDiagnosis("Gastritis");
+		treatment.setMedicine("Controloc");
+	}
+
+}
