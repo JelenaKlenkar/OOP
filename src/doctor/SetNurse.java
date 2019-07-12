768f2a38bbd830f1ad7bb9280ea66d839243839d
JelenaKlenkar <jelena.klenkar@gmail.com>
2019-07-12 22:52:36 +0200

    added Class SetNurse to package doctor

---
+++ src/doctor/SetNurse.java
@@ -0,0 +1,15 @@
+package doctor;
+
+public class SetNurse {
+
+	public static void main(String[] args) {
+		// create an object
+
+		Nurse nurse = new Nurse();
+
+		nurse.setFirstName("Whitney");
+		nurse.setLastName("Houston");
+		nurse.setPersonalIdentificationNumber(14567898);
+	}
+
+}
